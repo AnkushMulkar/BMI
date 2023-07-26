dcaf2b3bf3bd8aa0c9b87f374f25aa873072ebc6
ML ENGINEER <ankushmulkar.it@gmail.com>
2023-07-26 17:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image, ImageDraw, ImageOps
 import os
 
-
 def calculate_bmi(weight_kg, height_ft, height_in, gender):
     height_inch_total = height_ft * 12 + height_in
     height_m = height_inch_total * 0.0254
@@ -46,6 +45,42 @@ def get_food_recommendation(bmi_category):
     }
     return food_recommendations[bmi_category]
 
+def get_exercise_types(bmi_category):
+    exercise_types = {
+        "Underweight": ["Weightlifting", "Resistance training", "Yoga"],
+        "Normal weight": ["Cardio exercises", "Swimming", "Hiking"],
+        "Overweight": ["Running", "Cycling", "Dancing"],
+        "Obese": ["Low-impact aerobics", "Walking", "Pilates"]
+    }
+    return exercise_types[bmi_category]
+
+def get_yoga_types(bmi_category):
+    yoga_types = {
+        "Underweight": ["Bhujangasana (Cobra Pose)", "Vajrasana (Diamond Pose)", "Pavanamuktasana (Wind-Relieving Pose)"],
+        "Normal weight": ["Tadasana (Mountain Pose)", "Virabhadrasana (Warrior Pose)", "Balasana (Child's Pose)"],
+        "Overweight": ["Ardha Matsyendrasana (Half Lord of the Fishes Pose)", "Naukasana (Boat Pose)", "Utkatasana (Chair Pose)"],
+        "Obese": ["Baddha Konasana (Butterfly Pose)", "Setu Bandhasana (Bridge Pose)", "Sukhasana (Easy Pose)"]
+    }
+    return yoga_types[bmi_category]
+
+def get_pranayama(bmi_category):
+    pranayama = {
+        "Underweight": ["Kapalabhati Pranayama", "Bhastrika Pranayama", "Anulom Vilom Pranayama"],
+        "Normal weight": ["Sheetali Pranayama", "Bhramari Pranayama", "Nadi Shodhana Pranayama"],
+        "Overweight": ["Ujjayi Pranayama", "Surya Bhedana Pranayama", "Shitali Pranayama"],
+        "Obese": ["Bhramari Pranayama", "Nadi Shodhana Pranayama", "Kapalabhati Pranayama"]
+    }
+    return pranayama[bmi_category]
+
+def get_food_list(bmi_category):
+    food_list = {
+        "Underweight": ["Almonds", "Avocado", "Full-fat yogurt", "Quinoa", "Salmon"],
+        "Normal weight": ["Apples", "Broccoli", "Chicken breast", "Oats", "Spinach"],
+        "Overweight": ["Berries", "Cauliflower", "Lean turkey", "Brown rice", "Kale"],
+        "Obese": ["Chia seeds", "Lentils", "White fish", "Sweet potatoes", "Beets"]
+    }
+    return food_list[bmi_category]
+
 # Function to crop image in circular shape
 def crop_to_circle(image):
     width, height = image.size
@@ -67,13 +102,6 @@ def main():
     # Display the profile image in the sidebar
     st.sidebar.image(profile_image, use_column_width=True)
 
-    # Add background music from the current directory
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    audio_file_path = os.path.join(base_path, "music.mp3")
-    audio_file = open(audio_file_path, "rb")
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
     weight_kg = st.number_input("Enter your weight in kg", min_value=1.0, step=0.1)
     height_ft = st.number_input("Enter your height in feet", min_value=1, step=1)
     height_in = st.number_input("Enter the remaining height in inches", min_value=0, max_value=11, step=1)
@@ -90,13 +118,29 @@ def main():
         exercise_recommendation = get_exercise_recommendation(bmi_category)
         st.write(exercise_recommendation)
 
+        st.subheader("Exercise Types")
+        exercise_types = get_exercise_types(bmi_category)
+        st.write(exercise_types)
+
         st.subheader("Yoga Recommendation")
         yoga_recommendation = get_yoga_recommendation(bmi_category)
         st.write(yoga_recommendation)
 
+        st.subheader("Yoga Types")
+        yoga_types = get_yoga_types(bmi_category)
+        st.write(yoga_types)
+
+        st.subheader("Pranayama Recommendation")
+        pranayama_recommendation = get_pranayama(bmi_category)
+        st.write(pranayama_recommendation)
+
         st.subheader("Food Recommendation")
         food_recommendation = get_food_recommendation(bmi_category)
         st.write(food_recommendation)
+
+        st.subheader("Food List")
+        food_list = get_food_list(bmi_category)
+        st.write(food_list)
 
 if __name__ == "__main__":
     main()
